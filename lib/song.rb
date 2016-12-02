c5348db86f7bc8b1a990b6e81c98be1d5181248f
limerentfox <christi@limerentfox.com>
2016-12-02 03:57:41 -0500

    stripped whitespace

--- lib/song.rb
+++ lib/song.rb
@@ -1,67 +1,59 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+    attr_accessor :name, :artist_name
+    @@all = []
 
-  def self.all
-    @@all
-  end
-
-  def self.create
-    self.new.save
-  end
-
-  def artist_name=(name)
-    @artist_name = name
-  end
-
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
-  end
-
-  def self.create_by_name(name)
-    song = self.new_by_name(name)
-    song.save
-  end
-
-  def self.find_by_name(name)
-    self.all.detect {|song| song.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    self.all.each do |song|
-      if song.name == name
-        return song
-      end
+    def self.all
+        @@all
     end
-    song = self.create_by_name(name)
-  end
 
-  def self.alphabetical
-    self.all.sort_by {|song| song.name}
-  end
+    def self.create
+        new.save
+    end
 
-  def self.new_from_filename(filename)
-    song = self.new
-    song.name = filename.split(" - ")[1].strip.chomp(".mp3")
-    song.artist_name =  filename.split(" - ")[0].strip.chomp(".mp3")
-    song
-  end
+    def self.new_by_name(name)
+        song = new
+        song.name = name
+        song
+    end
 
-  def self.create_from_filename(filename)
-    song = self.new_from_filename(filename)
-    song.save
-  end
+    def self.create_by_name(name)
+        song = new_by_name(name)
+        song.save
+    end
 
-  def self.destroy_all
-    self.all.clear
-  end
+    def self.find_by_name(name)
+        all.detect { |song| song.name == name }
+    end
 
+    def self.find_or_create_by_name(name)
+        all.each do |song|
+            return song if song.name == name
+        end
+        song = create_by_name(name)
+    end
 
-  def save
-    self.class.all << self
-    self
-  end
+    def self.alphabetical
+        all.sort_by(&:name)
+    end
 
+    def self.new_from_filename(filename)
+        song = new
+        song.name = filename.split(' - ')[1].strip.chomp('.mp3')
+        song.artist_name = filename.split(' - ')[0].strip.chomp('.mp3')
+        song
+    end
+
+    def self.create_from_filename(filename)
+        song = new_from_filename(filename)
+        song.save
+    end
+
+    def self.destroy_all
+        all.clear
+    end
+
+    def save
+        self.class.all << self
+        self
+    end
 end
